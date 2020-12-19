1f7d099d798053e9a5f945e590538cc0ef8b72da
Szilard Orban <orbanszlrd@yahoo.com>
2020-12-19 22:16:58 +0100

    Install primeng and primeicons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'skeleton-angular';
+  title = 'Skeleton for Angular Applications';
 }
