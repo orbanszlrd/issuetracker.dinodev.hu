6cc63df13c003528ea14be11106d72a4759000f3
Szilard Orban <orbanszlrd@yahoo.com>
2020-12-21 07:36:44 +0100

    Refaxctor the loader component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+
+import * as AppActions from './store/actions/app.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Skeleton for Angular Applications';
+export class AppComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.store
+        .select<any>((state: any) => state)
+        .subscribe((state: any) => {
+          //          console.log(state);
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 
-import { LoaderComponent } from './components/loader/loader.component';
-import { MenubarComponent } from './components/menubar/menubar.component';
+import { PrimeModule } from '../prime/prime.module';
 
 import { SanitizeHtmlPipe } from './pipes/sanitize-html.pipe';
-import { PrimeModule } from '../prime/prime.module';
+
+import { LoaderComponent } from './components/loader/loader.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
 
 @NgModule({
   declarations: [LoaderComponent, SanitizeHtmlPipe, MenubarComponent],
