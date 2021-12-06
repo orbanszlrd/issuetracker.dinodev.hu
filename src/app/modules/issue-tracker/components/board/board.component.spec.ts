40a4e4399561f60910641f47022820cfd65d2d26
Orban Szilard <orbanszlrd@gmail.com>
2021-12-06 21:38:02 +0100

    Remove the hideSidebar()

--- src/app/modules/issue-tracker/components/board/board.component.spec.ts
+++ src/app/modules/issue-tracker/components/board/board.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { FirebaseModule } from 'src/app/modules/firebase/firebase.module';
+import { PrimeModule } from 'src/app/modules/prime/prime.module';
+import { initialState } from 'src/app/store/reducers/app.reducer';
 
 import { BoardComponent } from './board.component';
 
@@ -6,11 +12,17 @@ describe('BoardComponent', () => {
   let component: BoardComponent;
   let fixture: ComponentFixture<BoardComponent>;
 
+  const initialState = {
+    app: { isLoading: true, showSidebar: false },
+    issuetracker: { projects: [], boards: [] },
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BoardComponent ]
-    })
-    .compileComponents();
+      imports: [FormsModule, RouterTestingModule, FirebaseModule, PrimeModule],
+      providers: [provideMockStore({ initialState })],
+      declarations: [BoardComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
