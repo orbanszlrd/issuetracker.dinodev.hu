58596ef1101ca2d07808e1d024d570c31b340de9
Orban Szilard <orbanszlrd@gmail.com>
2021-12-07 18:53:06 +0100

    Create tests for the Projects component

--- src/app/modules/issue-tracker/components/projects/projects.component.spec.ts
+++ src/app/modules/issue-tracker/components/projects/projects.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 import { FirebaseModule } from 'src/app/modules/firebase/firebase.module';
 import { PrimeModule } from 'src/app/modules/prime/prime.module';
@@ -9,6 +10,9 @@ import { ProjectsComponent } from './projects.component';
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
   let fixture: ComponentFixture<ProjectsComponent>;
+
+  let spy: jasmine.Spy;
+  let store: Store;
 
   const initialState = {
     app: { isLoading: true, showSidebar: false },
@@ -21,6 +25,9 @@ describe('ProjectsComponent', () => {
       providers: [provideMockStore({ initialState })],
       declarations: [ProjectsComponent],
     }).compileComponents();
+
+    store = TestBed.inject(Store);
+    spy = spyOn(store, 'dispatch');
   });
 
   beforeEach(() => {
@@ -31,5 +38,62 @@ describe('ProjectsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open the new panel', () => {
+    expect(component.dialog.isOpen).toBeFalse();
+    component.newPanel();
+    expect(component.dialog.isOpen).toBeTrue();
+    expect(component.dialog.title).toEqual('New Project');
+  });
+
+  it('should close the new panel', () => {
+    component.dialog.isOpen = true;
+    component.closePanel();
+    expect(component.dialog.isOpen).toBeFalse();
+  });
+
+  it('should open the new panel', () => {
+    let project = {
+      id: '1',
+      title: 'Project  1',
+      slug: 'project-1',
+      description: '',
+    };
+
+    expect(component.dialog.isOpen).toBeFalse();
+
+    component.editPanel(project);
+
+    expect(component.dialog.isOpen).toBeTrue();
+    expect(component.dialog.title).toEqual('Edit Project');
+    expect(component.project.title).toEqual(project.title);
+  });
+
+  it('should call the dispatch method of the store on save', () => {
+    component.dialog.title = 'New Project';
+    spy.calls.reset();
+    component.save();
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call the dispatch method of the store on save ', () => {
+    component.dialog.title = 'Edit Project';
+    spy.calls.reset();
+    component.save();
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call the dispatch method of the store on delete', () => {
+    let project = {
+      id: '1',
+      title: 'Project  1',
+      slug: 'project-1',
+      description: '',
+    };
+
+    spy.calls.reset();
+    component.delete(project);
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
   });
 });
