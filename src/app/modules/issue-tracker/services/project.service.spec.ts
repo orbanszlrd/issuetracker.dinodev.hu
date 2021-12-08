a3777fd1b5c6279eeed31a9ef6af6a4faaedcaf0
Orban Szilard <orbanszlrd@gmail.com>
2021-12-08 13:19:41 +0100

    Create tests for the Project service

--- src/app/modules/issue-tracker/services/project.service.spec.ts
+++ src/app/modules/issue-tracker/services/project.service.spec.ts
@@ -1,19 +1,140 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable, of } from 'rxjs';
 import { FirebaseModule } from 'src/app/modules/firebase/firebase.module';
+import { Project } from '../models/project.model';
 
 import { ProjectService } from './project.service';
 
 describe('ProjectService', () => {
   let service: ProjectService;
 
+  const afsSpy = jasmine.createSpyObj('AngularFirestore', ['collection']);
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [FirebaseModule],
     });
-    service = TestBed.inject(ProjectService);
+    afsSpy.collection.and.returnValue({
+      valueChanges: () => of({}),
+      doc: (id: string) => {
+        return {
+          set: (project: Project) =>
+            new Promise((resolve, reject) => {
+              if (id) {
+                resolve();
+              } else {
+                reject();
+              }
+            }),
+          delete: () =>
+            new Promise((resolve, reject) => {
+              if (id) {
+                resolve();
+              } else {
+                reject();
+              }
+            }),
+        };
+      },
+    } as AngularFirestoreCollection);
+
+    service = new ProjectService(afsSpy);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should select the collection of projects', () => {
+    let collection = service.select();
+    expect(collection).toBeDefined();
+  });
+
+  it('should created a new project', (done) => {
+    let project: Project = {
+      title: 'Project 1',
+      slug: 'project-1',
+      description: '',
+    };
+
+    let result = service.create(project);
+
+    expect(result).toBeInstanceOf(Observable);
+
+    result.subscribe((data) => {
+      expect(data.title).toEqual(project.title);
+      done();
+    });
+  });
+
+  it('should fail to update the project if it has no id', (done) => {
+    let project: Project = {
+      title: 'Project 1',
+      slug: 'project-1',
+      description: '',
+    };
+
+    let result = service.update(project);
+
+    expect(result).toBeInstanceOf(Observable);
+
+    result.subscribe((data) => {
+      expect(data.title).toEqual(project.title);
+      done();
+    });
+  });
+
+  it('should update the project if it an id', (done) => {
+    let project: Project = {
+      id: '1',
+      title: 'Project 1',
+      slug: 'project-1',
+      description: '',
+    };
+
+    let result = service.update(project);
+
+    expect(result).toBeInstanceOf(Observable);
+
+    result.subscribe((data) => {
+      expect(data.title).toEqual(project.title);
+      done();
+    });
+  });
+
+  it('should fail to delete an project without providing the id', (done) => {
+    let project: Project = {
+      title: 'Project 1',
+      slug: 'project-1',
+      description: '',
+    };
+
+    let result = service.delete(project);
+
+    expect(result).toBeInstanceOf(Observable);
+
+    result.subscribe((data) => {
+      expect(data.title).toEqual(project.title);
+      done();
+    });
+  });
+
+  it('should delete the project with an id provided', (done) => {
+    let project: Project = {
+      id: '1',
+      title: 'Project 1',
+      slug: 'project-1',
+      description: '',
+    };
+
+    let result = service.delete(project);
+
+    expect(result).toBeInstanceOf(Observable);
+
+    result.subscribe((data) => {
+      expect(data.title).toEqual(project.title);
+      done();
+    });
   });
 });
