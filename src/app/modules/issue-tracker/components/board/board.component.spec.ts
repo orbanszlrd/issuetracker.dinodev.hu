8d6233f472afe5f15c067a9a152a1509395bdc4a
Orban Szilard <orbanszlrd@gmail.com>
2021-12-09 15:27:22 +0100

    Fix the tests of the Board component

--- src/app/modules/issue-tracker/components/board/board.component.spec.ts
+++ src/app/modules/issue-tracker/components/board/board.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
-import { FirebaseModule } from 'src/app/modules/firebase/firebase.module';
 import { PrimeModule } from 'src/app/modules/prime/prime.module';
-import { initialState } from 'src/app/store/reducers/app.reducer';
+import { Project } from '../../models/project.model';
 
 import { BoardComponent } from './board.component';
 
@@ -12,15 +12,48 @@ describe('BoardComponent', () => {
   let component: BoardComponent;
   let fixture: ComponentFixture<BoardComponent>;
 
+  const project: Project = {
+    id: '0000000000000000',
+    slug: 'project-101',
+    title: 'Project 101',
+    description: '',
+  };
+
+  const board = {
+    id: '111',
+    title: 'Board 111',
+    slug: 'board-111',
+    description: '',
+    projectId: project.id,
+    columns: [{ name: 'To do' }, { name: 'In Progress' }, { name: 'Done' }],
+  };
+
   const initialState = {
     app: { isLoading: true, showSidebar: false },
-    issuetracker: { projects: [], boards: [] },
+    issuetracker: { projects: { data: [project] }, boards: { data: [board] } },
   };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormsModule, RouterTestingModule, FirebaseModule, PrimeModule],
-      providers: [provideMockStore({ initialState })],
+      imports: [FormsModule, RouterTestingModule, PrimeModule],
+      providers: [
+        BoardComponent,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (param: string): string | void => {
+                  if (param === 'projectSlug') return project.slug;
+                  if (param === 'boardSlug') return board.slug;
+                  else return;
+                },
+              },
+            },
+          },
+        },
+        provideMockStore({ initialState }),
+      ],
       declarations: [BoardComponent],
     }).compileComponents();
   });
@@ -33,5 +66,13 @@ describe('BoardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load the correct project', () => {
+    expect(component.project).toEqual(project);
+  });
+
+  it('should load the correct board', () => {
+    expect(component.board).toEqual(board);
   });
 });
